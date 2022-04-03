16339c51db538e01fef1ee1b0e8ac29dfb755d3f
al-anas <alanas7171@gmail.com>
2022-04-04 03:00:21 +0600

    routes route notfound component included

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/home">Home</Link>
         <Link to="/reviews">Reviews</Link>
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/blogs">Blogs</Link>
