9deebd2f6c317db24afc2c8951cd3b6701f828b1
al-anas <alanas7171@gmail.com>
2022-04-04 03:16:16 +0600

    home component updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 const Home = () => {
-    return (
-        <div>
-            <h1>This is home page</h1>
+  return (
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col-lg-6 d-flex align-items-center">
+          <div>
+            <h1>Your Best Camera</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+              voluptas, ut vitae exercitationem voluptatem reprehenderit
+              quisquam! Quos impedit quisquam quas magnam maxime. Ea perferendis
+              itaque cupiditate rem quaerat veniam omnis.
+            </p>
+            <button className="btn btn-primary">Live Demo</button>
+          </div>
         </div>
-    );
+        <div className="col-lg-6">
+          <img
+            src="https://img.freepik.com/free-psd/camera-mockup_1310-717.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
